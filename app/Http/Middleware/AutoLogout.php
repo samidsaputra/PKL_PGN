92d80c74d1ffccb9955e115a13db7a1907d17284
samidsaputra <dimassaputracps@gmail.com>
2025-02-20 09:13:46 +0700

    Final

---
+++ app/Http/Middleware/AutoLogout.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+
+class AutoLogout
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::check()) {
+            $lastActivity = session('last_activity', now());
+
+            // Cek apakah sudah lebih dari 30 detik sejak aktivitas terakhir
+            if (Carbon::parse($lastActivity)->diffInSeconds(now()) > 30) {
+                Auth::logout();
+                session()->flush();
+                return redirect('/')->with('error', 'Session expired due to inactivity.');
+            }
+
+            // Perbarui waktu aktivitas terakhir di sesi
+            session(['last_activity' => now()]);
+        }
+
+        return $next($request);
+    }
+}
--- routes/request.php
+++ routes/request.php
@@ -4,14 +4,16 @@ use App\Http\Controllers\requestController;
 use App\Http\Controllers\addBarangController;
 use App\Http\Controllers\UserReg;
 use Illuminate\Support\Facades\Route;
+use \App\Http\Middleware\AutoLogout;
 
+Route::middleware([AutoLogout::class])->group(function () {
+    /** Request*/
+    Route::get('/req/request', [requestController::class, 'request'])->name('request.requester');
+    Route::post('/cart/checkout', [requestController::class, 'checkout'])->name('cart.checkout');
 
-/** Request*/
-Route::get('/req/request', [requestController::class, 'request'])->name('request.requester');
-Route::post('/cart/checkout', [requestController::class, 'checkout'])->name('cart.checkout');
-
-/** MyRequest */
-Route::get('/req/myrequest',[requestController::class, 'history'])->name('request.history');
-Route::get('/req/myrequest/detail/{noorder}',[requestController::class, 'show'])->name('request.show');
-Route::get('/req/myrequest/{noorder}/edit', [requestController::class, 'edit'])->name('requester.orders.edit');
-Route::post('/req/myrequest/{noorder}/update-order', [requestController::class, 'update'])->name('requester.orders.update');
+    /** MyRequest */
+    Route::get('/req/myrequest',[requestController::class, 'history'])->name('request.history');
+    Route::get('/req/myrequest/detail/{noorder}',[requestController::class, 'show'])->name('request.show');
+    Route::get('/req/myrequest/{noorder}/edit', [requestController::class, 'edit'])->name('requester.orders.edit');
+    Route::post('/req/myrequest/{noorder}/update-order', [requestController::class, 'update'])->name('requester.orders.update');
+});
